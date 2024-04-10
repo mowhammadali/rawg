ed1d70d2e27b996f1058f89564c2e4c3ca010f5b
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2024-04-10 19:42:57 +0330

    build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import { Grid, GridItem , Show } from '@chakra-ui/react'
+
 const App = () => {
     return (
-        <div>app</div>
+        <Grid templateAreas={{
+            base: `"nav" "main"`,
+            lg: `"nav nav" "aside main"`
+        }}>
+            <GridItem area="nav" bg="coral">
+                Nav
+            </GridItem>
+            <Show above="lg">
+                <GridItem area="aside" bg="gold">
+                    aside
+                </GridItem>
+            </Show>
+            <GridItem area="main" bg="dodgerblue">
+                main
+            </GridItem>
+        </Grid>
     )
 }
 
